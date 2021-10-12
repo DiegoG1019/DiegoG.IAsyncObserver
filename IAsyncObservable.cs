2805d8db0e3c57c4afca28a047f63822acee8d14
Diego García <dagarciam1014@gmail.com>
2021-10-12 18:21:58 -0500

    Add project files.

---
+++ IAsyncObservable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DiegoG.IAsyncObserver
+{ 
+    /// <summary>
+    /// Defines a provider for asynchronous push-based notifications
+    /// </summary>
+    /// <typeparam name="T">The object that provides notification information</typeparam>
+    public interface IAsyncObservable<out T>
+    {
+        /// <summary>
+        /// Notifies the provider that an observer is to receive notifications asynchronously.
+        /// </summary>
+        /// <param name="observer">The object that is to receive notifications.</param>
+        /// <returns>A reference to an interface that allows observers to stop receiving notifications before the provider has finished sending them</returns>
+        Task<IAsyncDisposable> Subscribe(IAsyncObserver<T> observer);
+    }
+}
---
+++ IAsyncObserver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoG.IAsyncObserver
+{
+    /// <summary>
+    /// Defines a mechanism for asynchronous push-based notifications
+    /// </summary>
+    /// <typeparam name="T">The object that provides notification information</typeparam>
+    public interface IAsyncObserver<in T>
+    {
+        /// <summary>
+        /// Notifies the observer that the provider has finished sending push-based notifications.
+        /// </summary>
+        Task OnCompleted();
+
+        /// <summary>
+        /// Notifies the observer that the provider has experienced an error condition.
+        /// </summary>
+        /// <param name="error"></param>
+        Task OnError(Exception error);
+
+        /// <summary>
+        /// Provides the observer with new data
+        /// </summary>
+        /// <param name="value">The current notification information.</param>
+        Task OnNext(T value);
+    }
+}
